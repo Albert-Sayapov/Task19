d1c28359b9dfb52b2e32bf0d18a498cdbd2d03fe
Albert Sayapov <asayapov@mail.ru>
2022-10-29 19:51:39 +0700

    queue.offer in while fix

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,20 @@ import java.util.Queue;
 
 public class Main {
 
-    //protected static Person person1 = new Person("Алексей", "Орлов", 4);
-    //protected static Person person2 = new Person("Роман", "Горлов", 7);
+    protected static Person person1 = new Person("Алексей", "Орлов", 4);
+    protected static Person person2 = new Person("Роман", "Горлов", 7);
     protected static Person person3 = new Person("Андрей", "Алтухов", 2);
-    //protected static Person person4 = new Person("Альберт", "Саяпов", 9);
-    //protected static Person person5 = new Person("Денис", "Анциборов", 5);
+    protected static Person person4 = new Person("Альберт", "Саяпов", 9);
+    protected static Person person5 = new Person("Денис", "Анциборов", 5);
 
     static List<Person> generateClients() {
         List<Person> persons = new LinkedList<>();
 
-        //persons.add(person1);
-        //persons.add(person2);
+        persons.add(person1);
+        persons.add(person2);
         persons.add(person3);
-        //persons.add(person4);
-        //persons.add(person5);
+        persons.add(person4);
+        persons.add(person5);
 
         return persons;
     }
